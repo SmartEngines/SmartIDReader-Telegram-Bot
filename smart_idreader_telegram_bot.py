4fbcfd268238e6c8f832b88fa7c91881ea4d6a0d
i-tolstov <56876645+i-tolstov@users.noreply.github.com>
2020-03-19 18:38:24 +0300

    actualization of smart_idreader_telegram_bot.py

--- smart_idreader_telegram_bot.py
+++ smart_idreader_telegram_bot.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2017, Smart Engines Ltd
+# Copyright (c) 2012-2020, Smart Engines Ltd
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without modification,
@@ -24,95 +24,121 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import pySmartIdEngine as se  # Smart IDReader by Smart Engines
-
-import telepot
-from telepot.delegate import per_chat_id, create_open, pave_event_space
-from telepot.loop import MessageLoop
-
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import json
 import time
 import os
+import sys
 from argparse import ArgumentParser
 
+import pySmartIdEngine as se  # Smart IDReader by Smart Engines
 
-class SmartIDReaderBot(telepot.helper.ChatHandler):
-    def __init__(self, seed_tuple, smartid_engine, **kwargs):
-        self.smartid_engine = smartid_engine
+class SmartIDReaderEngine():
+    def __init__(self, config_path):
+        self.smartid_engine = se.RecognitionEngine(config_path)
+    
+    def recognize_image_file(self, image_file_path):
+        # Create session settings and enable document types
+        session_settings = self.smartid_engine.CreateSessionSettings()
+        session_settings.AddEnabledDocumentTypes('rus.passport.national')
 
-        super(SmartIDReaderBot, self).__init__(seed_tuple, **kwargs)
+        # Create recognition session
+        session = self.smartid_engine.SpawnSession(session_settings)
 
-    def on_chat_message(self, msg):
-        try:
-            content_type, chat_type, chat_id = telepot.glance(msg)
+        # Recognize image file
+        result = session.ProcessImageFile(image_file_path)
 
-            if content_type in ['document', 'photo']:
-                content = msg[content_type] if content_type == 'document' else msg[content_type][-1]
-                if 'file_id' in content:
-                    # Creating downloads directory and downloading image file
-                    downloads_dir = 'downloaded_images'
-                    os.makedirs(downloads_dir, exist_ok=True)
-                    temp_path = os.path.join(downloads_dir,
-                                             'chat_%d_id_%d_temp.png' % (chat_id, msg['message_id']))
-                    self.bot.download_file(content['file_id'], temp_path)
+        # Convert string fields from recognition results to dictionary
+        recognized_fields = {}
+        for field_name in result.GetStringFieldNames():
+            field = result.GetStringField(field_name)
+            recognized_fields[field_name] = field.GetValue().GetUtf8String()
 
-                    # Recognizing it and sending message with result
-                    recognition_result_str = recognize_image_file(self.smartid_engine, temp_path)
-                    self.send_message(recognition_result_str)
-            else:
-                self.send_message("Please send me a photo and I'll recognize it!")
-        except Exception as e:
-            self.send_message('Exception: %s' % e.message)
+        # Return JSON dictionary as string
+        return json.dumps(recognized_fields, ensure_ascii=False, indent=2)
 
-    def send_message(self, message):
-        self.sender.sendMessage(message)
-        print(message)
+    def on_chat_photo(self, update, context):
+        # Creating downloads directory and downloading photo
+        temp_path = get_photo(update)
 
+        # Recognizing it and sending message with result
+        recognition_result_str = self.recognize_image_file(temp_path)
+        update.message.reply_text(recognition_result_str)
+        print(recognition_result_str)
+        os.remove(temp_path)
 
-def recognize_image_file(smartid_engine, image_file_path):
-    # Create session settings and enable document types
-    session_settings = smartid_engine.CreateSessionSettings()
-    session_settings.AddEnabledDocumentTypes('rus.passport.national')
+    def on_chat_image(self, update, context):
+        # Creating downloads directory and downloading image file
+        temp_path = get_image(update)
 
-    # Create recognition session
-    session = smartid_engine.SpawnSession(session_settings)
+        # Recognizing it and sending message with result
+        recognition_result_str = self.recognize_image_file(temp_path)
+        update.message.reply_text(recognition_result_str)
+        print(recognition_result_str)
+        os.remove(temp_path)
 
-    # Recognize image file
-    result = session.ProcessImageFile(image_file_path)
+def get_photo(update):
+    # Creating downloads directory and downloading image file
+    downloads_dir = 'downloaded_images'
+    os.makedirs(downloads_dir, exist_ok=True)
+    temp_path = os.path.join(downloads_dir,
+                                'file_%s_id_%d_temp.png' % (update.message.photo[-1].file_id, update.message.message_id))
+    update.message.photo[-1].get_file().download(temp_path)
+    return temp_path
 
-    # Convert string fields from recognition results to dictionary
-    recognized_fields = {}
-    for field_name in result.GetStringFieldNames():
-        field = result.GetStringField(field_name)
-        recognized_fields[field_name] = field.GetValue().GetUtf8String()
+def get_image(update):
+    # Creating downloads directory and downloading image file
+    downloads_dir = 'downloaded_images'
+    os.makedirs(downloads_dir, exist_ok=True)
+    temp_path = os.path.join(downloads_dir, update.message.document.file_name)
+    update.message.document.get_file().download(custom_path = temp_path)
+    return temp_path
 
-    # Return JSON dictionary as string
-    return json.dumps(recognized_fields, ensure_ascii=False, indent=2)
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text("Please send me a photo and I'll recognize it!")
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--token', type=str, required=True)
+    parser.add_argument('--token', type=str, )
     parser.add_argument('--smartid-config', type=str, default='bundle_mock_smart_idreader.zip')
     args = parser.parse_args()
 
-    # Creating recognition engine
-    smartid_engine = se.RecognitionEngine(args.smartid_config)
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
 
-    # Creating bot
-    bot = telepot.DelegatorBot(args.token, [
-        pave_event_space()(
-            per_chat_id(), create_open,
-            SmartIDReaderBot, smartid_engine, timeout=1000000
-        )
-    ])
 
-    # Starting bot
-    MessageLoop(bot).run_as_thread()
-    print('Bot started, listening to messages...')
+    updater = Updater(args.token, use_context=True)
 
-    while 1:
-        time.sleep(10)
+    # create an instance of SmartIdReader engine
+    smartid_engine = SmartIDReaderEngine(args.smartid_config)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # on photo - recognize it with SmartIdReader
+    dp.add_handler(MessageHandler(Filters.photo, smartid_engine.on_chat_photo))
+    dp.add_handler(MessageHandler(Filters.document.image, smartid_engine.on_chat_image))
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
 if __name__ == '__main__':
